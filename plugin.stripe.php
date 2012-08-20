5001f19c3362d8fbf3a5b934db7e658614ff12dc
Andrew Detwiler <adetwiler@adidamnetworks.com>
2012-08-20 05:21:29 +0000

    Updating Model name in plugin
    * Updates model name to use stripe instead of payments.

--- plugin.stripe.php
+++ plugin.stripe.php
@@ -2,7 +2,7 @@
 
 class Plugin_stripe extends Plugin {
     protected $models = array(
-        'payments' => array(
+        'stripe' => array(
             'stripe','stripe_card','stripe_charge','stripe_customer','stripe_event','stripe_invoice','stripe_plan',
         ),
     );
